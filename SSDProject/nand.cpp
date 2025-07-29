ee3e98ffc1d1bcad780b0ef8b5a2973edee4e820
jungjeeyoon <wjdwldbs1@skku.edu>
2025-07-29 15:51:01 +0900

    [#10][Feature] Nand Class 설계 및 Read 함수 구현

--- SSDProject/nand.cpp
+++ SSDProject/nand.cpp
@@ -1,1 +1,23 @@
+#ifdef _DEBUG
+#include <stdexcept>
+#endif
 #include "nand.h"
+
+bool Nand::read(int lba, int &out)
+{
+	if (lba < 0 || lba > 99)
+	{
+#ifdef _DEBUG
+		throw(std::exception("INVALID PARAMETER"));
+#else
+		return false;
+#endif
+	}
+
+	return true;
+	
+}
+bool Nand::write(int lba, int data)
+{
+	return 0;
+}
--- SSDProject/nand_test.cpp
+++ SSDProject/nand_test.cpp
@@ -2,4 +2,22 @@
 #include "gmock/gmock.h"
 #include "nand.h"
 
+class NandFixture : public testing::Test {
+public:
+    class Nand nand;
+};
+
+TEST_F(NandFixture, READ_INVALID_PARAMETER) {
+    int lba = 10000; // lba must be 0 to 99,
+    int out = 0;
+    EXPECT_THROW(nand.read(lba, out), std::exception);
+}
+
+TEST_F(NandFixture, READ_VALID_PARAMETER)
+{
+    int lba = 50; //lba must be below 50
+    int out = 0;
+    EXPECT_EQ(nand.read(lba, out), true);
+}
+
 #endif
