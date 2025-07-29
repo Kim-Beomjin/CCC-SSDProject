180a5f18fe85a097a47dad639db61e285583a2c2
yunzhen-kim <yunzhen.kim@gmail.com>
2025-07-29 16:55:36 +0900

    add executor skeleton code

---
+++ TestShell/executor.cpp
@@ -0,0 +1,52 @@
+#include <string>
+
+#define interface struct
+
+interface Executor {
+	virtual bool execute(const char* command, const char* args) = 0;
+};
+
+class Writor : public Executor {
+public:
+	bool execute(const char* command, const char* args) override {
+		return true;
+	}
+};
+
+class Reader : public Executor {
+public:
+	bool execute(const char* command, const char* args) override {
+		return true;
+	}
+};
+
+class Helper : public Executor {
+public:
+	bool execute(const char* command, const char* args) override {
+		return true;
+	}
+};
+
+class Exitor : public Executor {
+public:
+	bool execute(const char* command, const char* args) override {
+		// TODO: print exit message
+		return true;
+	}
+};
+
+class ExecutorFactory {
+public:
+	Executor* createExecutor(const std::string command) {
+		if (command == "write" || command == "fullwrite") {
+			return new Writor();
+		} else if (command == "read" || command == "fullread") {
+			return new Reader();
+		} else if (command == "help") {
+			return new Helper();
+		} else if (command == "exit") {
+			return new Exitor();
+		}
+		return nullptr;
+	}
+};
