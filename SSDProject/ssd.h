6abe11ad920bccc2c1f654785a3ebadd9e63a547
kgm-dsedu2 <kimkm18@gmail.com>
2025-07-29 16:42:19 +0900

    [#27][GREEN] SSD class 생성 및 구현

--- SSDProject/ssd.h
+++ SSDProject/ssd.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include "nand.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class SSD
+{
+public:
+  SSD(NandInterface* nand) : nand {nand}{};
+  bool read(int lba);
+  bool write(int lba, int data);
+
+private:
+  NandInterface* nand;
+};
--- SSDProject/ssd_test.cpp
+++ SSDProject/ssd_test.cpp
@@ -1,36 +1,66 @@
 #ifdef _DEBUG
 #include "gmock/gmock.h"
 #include "ssd.h"
-#include <iostream>
-#include <fstream>
-#include <string>
+
+using namespace testing;
+
+using ::testing::_;
+using ::testing::Invoke;
+using ::testing::DoAll;
+using ::testing::SetArgReferee;
+
+class MockNand : public NandInterface
+{
+public:
+  MOCK_METHOD(bool, Read, (const int lba, int& out));
+  MOCK_METHOD(bool, Write, (const int lba, const int data));
+};
 
 TEST(TC, ReadAfterWrite)
 {
-  SSD ssd;
-  unsigned int lba = 5;
-  unsigned int writeData = 0xdeadcafe;
+  MockNand* mockNand = new MockNand();
+  SSD ssd(mockNand);
+  int lba = 5;
+  int writeData = 0xdeadcafe;
   std::string outputFile = "ssd_output.txt";
 
+  ON_CALL(*mockNand, Read(_, _))
+    .WillByDefault(Invoke([=](const int, int& out) -> bool
+  {
+    out = writeData;
+    return true;
+  }));
+
   ssd.write(lba, writeData);
+
+
   ssd.read(lba);
   
   std::ifstream ifs(outputFile);
-  
+
   std::string readData((std::istreambuf_iterator<char>(ifs)),
                     std::istreambuf_iterator<char>());
   ifs.close();
 
-  EXPECT_EQ(writeData, readData);
+  EXPECT_EQ((writeData), (stoi(readData)));
+
+  delete mockNand;
 }
 
 TEST(TC, ReadWithoutWrite)
 {
-  SSD ssd;
-  unsigned int lba = 5;
-  unsigned int expectData = 0x0;
+  MockNand* mockNand = new MockNand();
+  SSD ssd(mockNand);
+  int lba = 5;
+  int expectData = 0x0;
   std::string outputFile = "ssd_output.txt";
 
+  ON_CALL(*mockNand, Read(_, _))
+    .WillByDefault(Invoke([=](const int lba, int& out) -> bool
+  {
+    out = expectData;
+    return true;
+  }));
   ssd.read(lba);
   
   std::ifstream ifs(outputFile);
@@ -39,23 +69,31 @@ TEST(TC, ReadWithoutWrite)
                     std::istreambuf_iterator<char>());
   ifs.close();
 
-  EXPECT_EQ(expectData, readData);
+  EXPECT_EQ(expectData, stoi(readData));
+
+  delete mockNand;
 }
 
 TEST(TC, ReadInvalidParam)
 {
-  SSD ssd;
-  unsigned int lba = 101;
+  MockNand* mockNand = new MockNand();
+  SSD ssd(mockNand);
+  int lba = 101;
 
   EXPECT_THROW(ssd.read(lba), std::exception);
+
+  delete mockNand;
 }
 
 TEST(TC, WriteInvalidParam)
 {
-  SSD ssd;
-  unsigned int lba = 101;
-  unsigned int writeData = 0xdeadcafe;
+  MockNand* mockNand = new MockNand();
+  SSD ssd(mockNand);
+  int lba = 101;
+  int writeData = 0xdeadcafe;
 
-  EXPECT_THROW(ssd.write(lba, writeData), std::exception);
+  EXPECT_THROW(std::to_string(ssd.write(lba, writeData)), std::exception);
+
+  delete mockNand;
 }
 #endif
