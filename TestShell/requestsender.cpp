96bf8163a3965d27ddea58361429bbd6f2c4c2c7
yunzhen-kim <yunzhen.kim@gmail.com>
2025-07-29 15:29:03 +0900

    [GREEN] make ReceiveCommand and SendRequest func

--- TestShell/requestsender.cpp
+++ TestShell/requestsender.cpp
@@ -9,11 +9,33 @@ class RequestSender {
 public:
 	RequestSender() : program_name_(""), command_(""), addr_(0), data_(0) {}
 	void ReceiveCommand(const string& program_name, const string& command, unsigned int addr, unsigned int data) {
-
+		program_name_ = program_name;
+		command_ = command;
+		addr_ = addr;
+		data_ = data;
+		if (command != "W" && command != "R") {
+			cout << "INVALID_COMMAND: " << command << endl;
+		}
 	}
 
 	int SendRequest() {
 		return 0;
+	}
+
+	const string& GetProgramName() const {
+		return program_name_;
+	}
+
+	const string& GetCommand() const {
+		return command_;
+	}
+
+	unsigned int GetAddress() const {
+		return addr_;
+	}
+
+	unsigned int GetData() const {
+		return data_;
 	}
 private:
 	string program_name_;
