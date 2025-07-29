c10dbcb4797466d5560c3184237f8249f9c42b89
jungjeeyoon <wjdwldbs1@skku.edu>
2025-07-29 17:06:16 +0900

    [#21][TDD_Refactor][FIX1] NAND Read/Write에 대한 Refactoring 진행 #30 -

--- SSDProject/nand_test.cpp
+++ SSDProject/nand_test.cpp
@@ -7,21 +7,35 @@ public:
     class Nand nand;
     
     int readData = 0;
-    const int VALID_LBA_ADDR = 50;
-    const int INVALID_LBA_ADDR = 10000;
+    static const int TEST_SIZE = 5;
+    const int VALID_LBA_ADDR_LIST[TEST_SIZE] = { 0, 50, 70, 90, 99 };
+    const int INVALID_LBA_ADDR_LIST[TEST_SIZE] = { -100, -10, -1, 100, 10000 };
     const int WRITE_DATA = 0xFFFF;
-
 };
 
-TEST_F(NandFixture, READ_VALID_AND_INVALID_PARAMETER_TEST) {
-    EXPECT_THROW(nand.Read(INVALID_LBA_ADDR, readData), std::exception);
-    EXPECT_EQ(nand.Read(VALID_LBA_ADDR, readData), true);
+TEST_F(NandFixture, READ_VALID_PARAMETER_TEST) {
+    for (int lba : VALID_LBA_ADDR_LIST) {
+        EXPECT_EQ(nand.Read(lba, readData), true);
+    }
 }
 
-TEST_F(NandFixture, WRITE_VALID_AND_INVALID_PARAMETER_TEST) {
-    EXPECT_THROW(nand.Write(INVALID_LBA_ADDR, WRITE_DATA), std::exception);
-    EXPECT_EQ(nand.Write(VALID_LBA_ADDR, WRITE_DATA), true);
+TEST_F(NandFixture, READ_INVALID_PARAMETER_TEST) {
+    for (int lba : INVALID_LBA_ADDR_LIST) {
+        EXPECT_THROW(nand.Read(lba, readData), std::exception);
+    }
 }
 
+TEST_F(NandFixture, WRITE_VALID_PARAMETER_TEST) {
+    for (int lba : VALID_LBA_ADDR_LIST) {
+        EXPECT_EQ(nand.Write(lba, WRITE_DATA), true);
+    }
+
+}
+
+TEST_F(NandFixture, WRITE_INVALID_PARAMETER_TEST) {
+    for (int lba : INVALID_LBA_ADDR_LIST) {
+        EXPECT_THROW(nand.Write(lba, WRITE_DATA), std::exception);
+    }
+}
 
 #endif
