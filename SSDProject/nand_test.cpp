3b3fa1cec188a16ad0d2abf653d79d0618b6b2e2
jungjeeyoon <wjdwldbs1@skku.edu>
2025-07-30 08:28:15 +0900

    [#21][TDD_Refactor][FIX2] NAND Read/Write에 대한 Refactoring 진행

--- SSDProject/nand_test.cpp
+++ SSDProject/nand_test.cpp
@@ -1,15 +1,16 @@
 #ifdef _DEBUG
 #include "gmock/gmock.h"
 #include "nand.h"
+#include <vector>
 
 class NandFixture : public testing::Test {
-public:
+protected:
     class Nand nand;
     
     int readData = 0;
     static const int TEST_SIZE = 5;
-    const int VALID_LBA_ADDR_LIST[TEST_SIZE] = { 0, 50, 70, 90, 99 };
-    const int INVALID_LBA_ADDR_LIST[TEST_SIZE] = { -100, -10, -1, 100, 10000 };
+    const std::vector<int> VALID_LBA_ADDR_LIST = { 0, 50, 70, 90, 99 };
+    const std::vector<int> INVALID_LBA_ADDR_LIST = { -100, -10, -1, 100, 10000 };
     const int WRITE_DATA = 0xFFFF;
 };
 
@@ -29,7 +30,6 @@ TEST_F(NandFixture, WRITE_VALID_PARAMETER_TEST) {
     for (int lba : VALID_LBA_ADDR_LIST) {
         EXPECT_EQ(nand.Write(lba, WRITE_DATA), true);
     }
-
 }
 
 TEST_F(NandFixture, WRITE_INVALID_PARAMETER_TEST) {
