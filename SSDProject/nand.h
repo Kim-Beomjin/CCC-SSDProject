68ea6064751a4260f399a4d3156271b8bbfea5ff
kgm-dsedu2 <kimkm18@gmail.com>
2025-07-29 16:42:19 +0900

    [#27][GREEN] SSD class 생성 및 구현

--- SSDProject/nand.h
+++ SSDProject/nand.h
@@ -1,13 +1,20 @@
 #pragma once
 
-class Nand
+class NandInterface
 {
 public:
-	Nand() {}
-	bool Read(const int lba, int &out);
-	bool Write(const int lba, const int data);
+  virtual bool Read(const int lba, int& out) = 0;
+  virtual bool Write(const int lba, const int data) = 0;
+};
+
+class Nand : public NandInterface
+{
+public:
+  Nand() {}
+  bool Read(const int lba, int& out);
+  bool Write(const int lba, const int data);
 private:
-	bool _IsValidParameter(const int lba);
-	const int LBA_START_ADDR = 0;
-	const int LBA_END_ADDR = 100;
+  bool _IsValidParameter(const int lba);
+  const int LBA_START_ADDR = 0;
+  const int LBA_END_ADDR = 100;
 };
--- SSDProject/ssd.cpp
+++ SSDProject/ssd.cpp
@@ -1,1 +1,41 @@
 #include "ssd.h"
+
+bool SSD::read(int lba)
+{
+  if (lba > 99)
+  {
+#ifdef _DEBUG
+    throw(std::exception("INVALID PARAMETER"));
+#endif
+    return false;
+  }
+
+  bool result;
+  int data;
+  std::string outputFile = "ssd_output.txt";
+
+  result = nand->Read(lba, data);
+  if (result)
+  {
+    std::ofstream ofs(outputFile);
+    
+    std::string stringData = std::to_string(data);
+    ofs.write(stringData.c_str(), stringData.size());
+    ofs.close();
+  }
+
+  return result;
+}
+
+bool SSD::write(int lba, int data)
+{
+  if (lba > 99)
+  {
+#ifdef _DEBUG
+    throw(std::exception("INVALID PARAMETER"));
+#endif
+    return false;
+  }
+
+  return nand->Write(lba, data);
+}
