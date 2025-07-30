1f161ccc68a7763dde40c2c23e319a85195152cf
Kim-Beomjin <kbkb2512@gmail.com>
2025-07-30 20:20:14 +0900

    [#101][TDD_Refactor]<<3_WriteReadAging>> phase 1 - Define Inheritance Relationship

--- TestShell/testScript.h
+++ TestShell/testScript.h
@@ -11,30 +11,55 @@ public:
 	TestScript() = default;
 	TestScript(Writer* writer, Reader* reader) : writer{ writer }, reader{ reader } {}
 
-	bool FullWriteAndReadCompare(string input, ISsdApp* app);
-	bool PartialLBAWrite(string input, ISsdApp* app);
-	bool WriteReadAging(string input, ISsdApp* app);
+	virtual bool execute(string input, ISsdApp* app) = 0;
+	virtual bool IsValidTestScriptName(std::string& input) = 0;
 
-	const string FIRST_TEST_SCRIPT_NAME_SHORT = "1_";
-	const string FIRST_TEST_SCRIPT_NAME_FULL = "1_FullWriteAndReadCompare";
-	static const int FIRST_TEST_SCRIPT_LOOP_COUNT = 20;
-	static const int FIRST_TEST_SCRIPT_LOOP_LBA = 5;
-
-	const string SECOND_TEST_SCRIPT_NAME_SHORT = "2_";
-	const string SECOND_TEST_SCRIPT_NAME_FULL = "2_PartialLBAWrite";
-	static const int SECOND_TEST_SCRIPT_LOOP_COUNT = 30;
-	static const int SECOND_TEST_SCRIPT_LOOP_LBA = 5;
-
-	const string THIRD_TEST_SCRIPT_NAME_SHORT = "3_";
-	const string THIRD_TEST_SCRIPT_NAME_FULL = "3_WriteReadAging";
-	static const int THIRD_TEST_SCRIPT_LOOP_COUNT = 200;
-	static const int THIRD_TEST_SCRIPT_LOOP_LBA = 2;
-
-private:
-	bool IsValidTestScript1Name(std::string& input);
-	bool IsValidTestScript2Name(std::string& input);
-	bool IsValidTestScript3Name(std::string& input);
-
+protected:
 	Writer* writer;
 	Reader* reader;
+};
+
+class FullWriteAndReadCompare : public TestScript {
+public:
+	FullWriteAndReadCompare() = default;
+	FullWriteAndReadCompare(Writer* writer, Reader* reader) : TestScript{ writer, reader } {}
+
+	static const int LOOP_COUNT = 20;
+	static const int NUM_LBA_PER_LOOP = 5;
+
+	const string SHORT_NAME = "1_";
+	const string FULL_NAME = "1_FullWriteAndReadCompare";
+
+	bool execute(string input, ISsdApp* app) override;
+	bool IsValidTestScriptName(std::string& input) override;
+};
+
+class PartialLBAWrite : public TestScript {
+public:
+	PartialLBAWrite() = default;
+	PartialLBAWrite(Writer* writer, Reader* reader) : TestScript{ writer, reader } {}
+
+	static const int LOOP_COUNT = 30;
+	static const int NUM_LBA_PER_LOOP = 5;
+
+	const string SHORT_NAME = "2_";
+	const string FULL_NAME = "2_PartialLBAWrite";
+
+	bool execute(string input, ISsdApp* app) override;
+	bool IsValidTestScriptName(std::string& input) override;
+};
+
+class WriteReadAging : public TestScript {
+public:
+	WriteReadAging() = default;
+	WriteReadAging(Writer* writer, Reader* reader) : TestScript{ writer, reader } {}
+
+	static const int LOOP_COUNT = 200;
+	static const int NUM_LBA_PER_LOOP = 2;
+
+	const string SHORT_NAME = "3_";
+	const string FULL_NAME = "3_WriteReadAging";
+
+	bool execute(string input, ISsdApp* app) override;
+	bool IsValidTestScriptName(std::string& input) override;
 };
