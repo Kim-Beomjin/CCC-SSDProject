4a2fcf108870bc9b68dd5862e9d62444b1faf2a9
yulgon <younggon5548@gmail.com>
2025-07-30 10:07:21 +0900

    [#45][TDD] add WriteAndExit / ReadAndExit TC

--- TestShell/shellInterface_test.cpp
+++ TestShell/shellInterface_test.cpp
@@ -34,4 +34,48 @@ TEST_F(ShellFixture, ExitCommand) {
     EXPECT_EQ(EXIT_RESULT, result);
 }
 
+TEST_F(ShellFixture, WriteAndExit) {
+    string fakeCmd = "write 3 0x12345678\nexit";
+    string WRITEANDEXIT_RESULT = "Shell> write 3 0x12345678\nShell> exit\n";
+    istringstream iss(fakeCmd);
+    ostringstream oss;
+
+    auto* oldInputBuf = cin.rdbuf();
+    streambuf* oldOutputBuf = cout.rdbuf();
+
+    std::cin.rdbuf(iss.rdbuf());
+    std::cout.rdbuf(oss.rdbuf());
+
+    shell.RunShellLoop();
+
+    std::cin.rdbuf(oldInputBuf);
+    std::cout.rdbuf(oldOutputBuf);
+
+    std::string result = oss.str();
+    //std::cout << "output" << result;
+    EXPECT_EQ(WRITEANDEXIT_RESULT, result);
+}
+
+TEST_F(ShellFixture, ReadAndExit) {
+    string fakeCmd = "read 3\nexit";
+    string READANDEXIT_RESULT = "Shell> read 3\nShell> exit\n";
+    istringstream iss(fakeCmd);
+    ostringstream oss;
+
+    auto* oldInputBuf = cin.rdbuf();
+    streambuf* oldOutputBuf = cout.rdbuf();
+
+    std::cin.rdbuf(iss.rdbuf());
+    std::cout.rdbuf(oss.rdbuf());
+
+    shell.RunShellLoop();
+
+    std::cin.rdbuf(oldInputBuf);
+    std::cout.rdbuf(oldOutputBuf);
+
+    std::string result = oss.str();
+    //std::cout << "output" << result;
+    EXPECT_EQ(READANDEXIT_RESULT, result);
+}
+
 #endif
