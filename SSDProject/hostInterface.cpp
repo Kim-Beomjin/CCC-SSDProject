ef21706acfcdff27bb5fc5e1bb1852755eaf2dab
jungjeeyoon <wjdwldbs1@skku.edu>
2025-07-30 19:48:08 +0900

    [#77] [TDD][SSD][REFACTOR] SSD 쪽 Host Interface layer 설계

--- SSDProject/hostInterface.cpp
+++ SSDProject/hostInterface.cpp
@@ -3,55 +3,91 @@
 
 void HostInterface::Execute(int argc, char* argv[])
 {
-	LBA lba = 0;
-	DATA data = 0;
-	if (argc == 4 && argv[1] == "W")
+
+	if (_WriteCondition(argc,argv))
 	{
-		DEBUG_ASSERT(argv[2][0] != '-', "LBA CANNOT BE NEGATIVE");
-		if (strlen(argv[3]) != 10)
-		{
-			DEBUG_ASSERT(false, "INVALID_WRITE_DATA_SIZE");
-			return;
-		}
-		
-		try {
-			size_t idx;
-			lba = std::stoul(argv[2], &idx, 0);
-			if (idx != strlen(argv[2])) {
-				throw std::invalid_argument("Invalid characters found in input");
-			}
-			
-			data = std::stoul(argv[3], &idx, 0);
-			if (idx != strlen(argv[3])) {
-				throw std::invalid_argument("Invalid characters found in input");
-			}
-		}
-		catch(std::exception)
-		{
-			DEBUG_ASSERT(false, "INVALID INPUT TYPE");
+		if (_LoadWriteParameterAndCheckInvalid(argv[2], argv[3])) {
 			return;
 		}
 		ssd->write(lba, data);
-		
 	}
-	else if (argc == 3 && argv[1] == "R")
+	else if (_ReadCondition(argc, argv))
 	{
-		DEBUG_ASSERT(argv[2][0] != '-', "LBA CANNOT BE NEGATIVE");
-		try {
-			size_t idx;
-			lba = std::stoul(argv[2], &idx, 0);
-			if (idx != strlen(argv[2])) {
-				throw std::invalid_argument("Invalid characters found in input");
-			}
-		}
-		catch (std::exception)
-		{
-			DEBUG_ASSERT(false, "INVALID INPUT TYPE");
+		if (_LoadReadParameterAndCheckInvalid(argv[2])) {
 			return;
 		}
 		ssd->read(lba);
 	}
 	else {
-		DEBUG_ASSERT(false, "INVALID INPUT TYPE");
+		DEBUG_ASSERT(false, "INVALID INPUT PARAMETERS");
 	}
 }
+
+bool HostInterface::_WriteCondition(int argc, char* argv[])
+{
+	return (argc == 4 && argv[1] == "W");
+}
+
+bool HostInterface::_ReadCondition(int argc, char* argv[])
+{
+	return (argc == 3 && argv[1] == "R");
+}
+
+
+bool HostInterface::_LoadWriteParameterAndCheckInvalid(char* lbaStr, char* dataStr)
+{
+	if (_IsNegative(lbaStr) || _IsInvalidLength(dataStr))
+	{
+		DEBUG_ASSERT(false, "INVALID INPUT PARAMETERS");
+		return false;
+	}
+	try {
+		lba = _SafeStoul(lbaStr);
+		data = _SafeStoul(dataStr);
+	}
+	catch (std::exception)
+	{
+		DEBUG_ASSERT(false, "INVALID INPUT PARAMETERS");
+		return false;
+	}
+	return true;
+}
+
+bool HostInterface::_LoadReadParameterAndCheckInvalid(char* lbaStr)
+{
+	if (_IsNegative(lbaStr))
+	{
+		DEBUG_ASSERT(false, "INVALID PARAMETER");
+		return false;
+	}
+	try {
+		lba = _SafeStoul(lbaStr);
+	}
+	catch (std::exception)
+	{
+		DEBUG_ASSERT(false, "INVALID INPUT TYPE");
+		return false;
+	}
+	return true;
+}
+
+bool HostInterface::_IsNegative(char* lbaStr)
+{
+	return (lbaStr[0] == '-');
+}
+
+bool HostInterface::_IsInvalidLength(char* dataStr)
+{
+	return (strlen(dataStr) != 10);
+}
+
+unsigned int HostInterface::_SafeStoul(char* str)
+{
+	size_t idx;
+	unsigned int ret = std::stoul(str, &idx, 0);
+	if (idx != strlen(str)) {
+		DEBUG_ASSERT(false, "INVALID INPUT TYPE");
+	}
+	return ret;
+}
+
--- SSDProject/hostInterface.h
+++ SSDProject/hostInterface.h
@@ -10,6 +10,7 @@ public:
     }
 
     void Execute(int argc, char* argv[]);
+
 #if _DEBUG
     void SetSSD(SSD* ssd)
     {
@@ -18,6 +19,15 @@ public:
 #endif
 
 private:
-    HostInterface() : ssd{ new SSD() } {};
+    HostInterface() : ssd{ new SSD() }, lba{ 0 }, data{ 0 } {};
+    bool _WriteCondition(int argc, char* argv[]);
+    bool _ReadCondition(int argc, char* argv[]);
+    bool _LoadWriteParameterAndCheckInvalid(char* lbaStr, char* dataStr);
+    bool _LoadReadParameterAndCheckInvalid(char* lbaStr);
+    bool _IsNegative(char* lbaStr);
+    bool _IsInvalidLength(char* dataStr);
+    unsigned int _SafeStoul(char* str);
+    LBA lba;
+    DATA data;
     SSD* ssd;
 };
