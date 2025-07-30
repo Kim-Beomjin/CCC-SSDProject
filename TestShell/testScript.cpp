a02437e48639c1f9c2f1560ccd5c5e6db5f0632f
Kim-Beomjin <kbkb2512@gmail.com>
2025-07-30 19:07:08 +0900

    [#90][TDD_Green]<<2_PartialLBAWrite>> phase 1 - Implement PartialLBAWrite()

--- TestShell/testScript.cpp
+++ TestShell/testScript.cpp
@@ -5,7 +5,7 @@
 
 bool TestScript::FullWriteAndReadCompare(string input, ISsdApp* app)
 {
-	if (IsValidTestScriptName(input) == false)
+	if (IsValidTestScript1Name(input) == false)
 	{
 #ifdef _DEBUG
 		throw runtime_error("Invalid Test Script");
@@ -33,7 +33,43 @@ bool TestScript::FullWriteAndReadCompare(string input, ISsdApp* app)
 	return true;
 }
 
-bool TestScript::IsValidTestScriptName(std::string& input)
+bool TestScript::PartialLBAWrite(string input, ISsdApp* app)
+{
+	if (IsValidTestScript2Name(input) == false)
+	{
+#ifdef _DEBUG
+		throw runtime_error("Invalid Test Script");
+#endif
+		return false;
+	}
+
+	int writeLba[5] = { 4, 0, 3, 1, 2 };
+	int readStartLba = 0;
+	int readEndLba = SECOND_TEST_SCRIPT_LOOP_LBA;
+
+	for (int loop = 0; loop < SECOND_TEST_SCRIPT_LOOP_COUNT; loop++)
+	{
+
+		for (auto lba : writeLba)
+		{
+			writer->execute("write", lba, lba, app);
+		}
+
+		for (int lba = readStartLba; lba < readEndLba; lba++)
+		{
+			int data = 0;
+			reader->execute("read", lba, data, app);
+		}
+	}
+}
+
+bool TestScript::IsValidTestScript1Name(std::string& input)
 {
 	return (input == "1_") || (input == "1_FullWriteAndReadCompare");
 }
+
+bool TestScript::IsValidTestScript2Name(std::string& input)
+{
+	return (input == "2_") || (input == "2_PartialLBAWrite");
+}
+
--- TestShell/testScript.h
+++ TestShell/testScript.h
@@ -12,12 +12,18 @@ public:
 	TestScript(Writer* writer, Reader* reader) : writer{ writer }, reader{ reader } {}
 
 	bool FullWriteAndReadCompare(string input, ISsdApp* app);
+	bool PartialLBAWrite(string input, ISsdApp* app);
+
 
 private:
-	bool IsValidTestScriptName(std::string& input);
+	bool IsValidTestScript1Name(std::string& input);
+	bool IsValidTestScript2Name(std::string& input);
 
 	const int FIRST_TEST_SCRIPT_LOOP_COUNT = 20;
 	const int FIRST_TEST_SCRIPT_LOOP_LBA = 5;
+
+	const int SECOND_TEST_SCRIPT_LOOP_COUNT = 30;
+	const int SECOND_TEST_SCRIPT_LOOP_LBA = 5;
 
 	Writer* writer;
 	Reader* reader;
