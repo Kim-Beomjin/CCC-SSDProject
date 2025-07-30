0be07b709b30bcf7232ce8928c11d0dc9b119c5c
yulgon <younggon5548@gmail.com>
2025-07-30 11:18:30 +0900

    [#45][TDD] add mock for exit/read/write tc

--- TestShell/shellInterface_test.cpp
+++ TestShell/shellInterface_test.cpp
@@ -7,9 +7,16 @@
 using namespace std;
 using namespace testing;
 
+class MockCommandParser : public CommandParserBridge {
+public:
+    MOCK_METHOD(bool, ParseCommand, (string cmd));
+};
+
 class ShellFixture : public Test {
 public:
+    NiceMock<MockCommandParser> mockCommandParser;
     class Shell shell;
+    class Shell mockshell {&mockCommandParser};
     string EXIT_CMD = "exit\n";
     string WRITE_CMD = "write 3 0x12345678\n";
     string READ_CMD = "read 3\n";
@@ -28,15 +35,15 @@ TEST_F(ShellFixture, ExitCommand) {
     auto* oldInputBuf = cin.rdbuf();
     streambuf* oldOutputBuf = cout.rdbuf();
 
-    std::cin.rdbuf(iss.rdbuf());
-    std::cout.rdbuf(oss.rdbuf());
+    cin.rdbuf(iss.rdbuf());
+    cout.rdbuf(oss.rdbuf());
 
     shell.RunShellLoop();
 
-    std::cin.rdbuf(oldInputBuf);
-    std::cout.rdbuf(oldOutputBuf);
+    cin.rdbuf(oldInputBuf);
+    cout.rdbuf(oldOutputBuf);
 
-    std::string result = oss.str();
+    string result = oss.str();
     //std::cout << "output" << result;
     EXPECT_EQ(EXIT_RESULT, result);
 }
@@ -50,15 +57,15 @@ TEST_F(ShellFixture, WriteAndExit) {
     auto* oldInputBuf = cin.rdbuf();
     streambuf* oldOutputBuf = cout.rdbuf();
 
-    std::cin.rdbuf(iss.rdbuf());
-    std::cout.rdbuf(oss.rdbuf());
+    cin.rdbuf(iss.rdbuf());
+    cout.rdbuf(oss.rdbuf());
 
     shell.RunShellLoop();
 
-    std::cin.rdbuf(oldInputBuf);
-    std::cout.rdbuf(oldOutputBuf);
+    cin.rdbuf(oldInputBuf);
+    cout.rdbuf(oldOutputBuf);
 
-    std::string result = oss.str();
+    string result = oss.str();
     //std::cout << "output" << result;
     EXPECT_EQ(WRITEANDEXIT_RESULT, result);
 }
@@ -72,15 +79,96 @@ TEST_F(ShellFixture, ReadAndExit) {
     auto* oldInputBuf = cin.rdbuf();
     streambuf* oldOutputBuf = cout.rdbuf();
 
-    std::cin.rdbuf(iss.rdbuf());
-    std::cout.rdbuf(oss.rdbuf());
+    cin.rdbuf(iss.rdbuf());
+    cout.rdbuf(oss.rdbuf());
 
     shell.RunShellLoop();
 
-    std::cin.rdbuf(oldInputBuf);
-    std::cout.rdbuf(oldOutputBuf);
+    cin.rdbuf(oldInputBuf);
+    cout.rdbuf(oldOutputBuf);
 
-    std::string result = oss.str();
+    string result = oss.str();
+    //std::cout << "output" << result;
+    EXPECT_EQ(READANDEXIT_RESULT, result);
+}
+
+TEST_F(ShellFixture, MockExitCommand) {
+    string fakeCmd = EXIT_CMD;
+
+    istringstream iss(fakeCmd);
+    ostringstream oss;
+
+    auto* oldInputBuf = cin.rdbuf();
+    streambuf* oldOutputBuf = cout.rdbuf();
+
+    cin.rdbuf(iss.rdbuf());
+    cout.rdbuf(oss.rdbuf());
+
+    EXPECT_CALL(mockCommandParser, ParseCommand)
+        .Times(1)
+        .WillOnce(Return(true));
+
+    mockshell.RunShellLoop();
+
+    cin.rdbuf(oldInputBuf);
+    cout.rdbuf(oldOutputBuf);
+
+    string result = oss.str();
+    //std::cout << "output" << result;
+    EXPECT_EQ(EXIT_RESULT, result);
+}
+
+TEST_F(ShellFixture, MockWriteAndExit) {
+    string fakeCmd = WRITE_CMD + EXIT_CMD;
+    string WRITEANDEXIT_RESULT = WRITE_RESULT + EXIT_RESULT;
+
+    istringstream iss(fakeCmd);
+    ostringstream oss;
+
+    auto* oldInputBuf = cin.rdbuf();
+    streambuf* oldOutputBuf = cout.rdbuf();
+
+    cin.rdbuf(iss.rdbuf());
+    cout.rdbuf(oss.rdbuf());
+
+    EXPECT_CALL(mockCommandParser, ParseCommand)
+        .Times(2)
+        .WillRepeatedly(Return(true));
+
+    mockshell.RunShellLoop();
+
+    cin.rdbuf(oldInputBuf);
+    cout.rdbuf(oldOutputBuf);
+
+    string result = oss.str();
+    //std::cout << "output" << result;
+    EXPECT_EQ(WRITEANDEXIT_RESULT, result);
+}
+
+TEST_F(ShellFixture, MockReadAndExit) {
+    string fakeCmd = READ_CMD + EXIT_CMD;
+    string READANDEXIT_RESULT = READ_RESULT + EXIT_RESULT;
+
+    istringstream iss(fakeCmd);
+    ostringstream oss;
+
+    auto* oldInputBuf = cin.rdbuf();
+    streambuf* oldOutputBuf = cout.rdbuf();
+
+    cin.rdbuf(iss.rdbuf());
+    cout.rdbuf(oss.rdbuf());
+
+
+    EXPECT_CALL(mockCommandParser, ParseCommand)
+        .Times(2)
+        .WillRepeatedly(Return(true));
+
+    mockshell.RunShellLoop();
+
+    cin.rdbuf(oldInputBuf);
+    cout.rdbuf(oldOutputBuf);
+
+    string result = oss.str();
     //std::cout << "output" << result;
     EXPECT_EQ(READANDEXIT_RESULT, result);
 }
