e3dd241a2f2466ed3b0ad1994e8890bdd50c4735
jungjeeyoon <wjdwldbs1@skku.edu>
2025-07-30 09:25:29 +0900

    [#35][TDD] (Red) File 존재하는지 여부 확인하는 TC 추가

--- SSDProject/nand.h
+++ SSDProject/nand.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <string>
+#include <fstream>
+#define NAND_FILE_NAME ("ssd_nand.txt")
 
 class NandInterface
 {
+	virtual bool Read(const int lba, int &out) = 0;
+	virtual bool Write(const int lba, const int data) = 0;
+}
+
+{
 public:
+	bool Read (const int lba, int &out) override;
+	bool Write(const int lba, const int data) override;
   virtual bool Read(const int lba, int& out) = 0;
   virtual bool Write(const int lba, const int data) = 0;
 };
@@ -14,7 +24,7 @@ public:
   bool Read(const int lba, int& out);
   bool Write(const int lba, const int data);
 private:
-  bool _IsValidParameter(const int lba);
-  const int LBA_START_ADDR = 0;
-  const int LBA_END_ADDR = 100;
-};
+	bool _IsFileExists(const std::string& filename);
+	void _CreateFile(const std::string& filename);
+	const int LBA_START_ADDR = 0;
+	const int LBA_END_ADDR = 100;};
