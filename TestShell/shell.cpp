79717a42e15c474d7d508c82a1596a0bc9a611e8
yulgon <younggon5548@gmail.com>
2025-07-30 09:25:17 +0900

    [#29][TDD] testshell: change tc using fake input/output

--- TestShell/shell.cpp
+++ TestShell/shell.cpp
@@ -5,17 +5,19 @@ using namespace std;
 
 class Shell {
 public:
-    void LoopGetCommand(void) {
+    void RunShellLoop(void) {
         string cmd;
         do {
             cout << "Shell> ";
 
-            cin.ignore();
+
 
             getline(std::cin, cmd);
-
+            // cin.ignore();
             // Parse cmd
-
+#ifdef _DEBUG
+            cout << cmd << "\n";
+#endif
         } while (IsCmdExit(cmd) == false);
     }
 
