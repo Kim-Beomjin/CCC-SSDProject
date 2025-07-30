81959186bdb3ec2939beebd6034f6a11a65f5d33
Kim-Beomjin <kbkb2512@gmail.com>
2025-07-30 11:10:19 +0900

    [#52][TDD_Red]<<1_FullWriteAndReadCompare>> phase 2 - Add to Check Read and Write times TC

--- TestShell/testScript_test.cpp
+++ TestShell/testScript_test.cpp
@@ -6,17 +6,43 @@
 using namespace std;
 using namespace testing;
 
+class MockWriter : public Writer {
+public:
+	MOCK_METHOD(bool, execute, (string, int, int), (override));
+};
+
+class MockReader : public Reader {
+public:
+	MOCK_METHOD(bool, execute, (string, int, int), (override));
+};
+
+
 class TestScriptFixture : public Test {
 public:
-    TestScript app;
+	TestScript app;
+	TestScript mockApp{ &mockWriter, &mockReader };
 
 	const string BLANK_TEST_SCRIPT_NAME = "";
 	const string INVALID_TEST_SCRIPT_NAME = "123";
+	const string FIRST_TEST_SCRIPT_NAME = "1_";
+
+	NiceMock<MockWriter> mockWriter;
+	NiceMock<MockReader> mockReader;
 };
 
 TEST_F(TestScriptFixture, ThrowInvalidTestScript) {
 	EXPECT_THROW(app.FullWriteAndReadCompare(BLANK_TEST_SCRIPT_NAME), runtime_error);
 	EXPECT_THROW(app.FullWriteAndReadCompare(INVALID_TEST_SCRIPT_NAME), runtime_error);
+}
+
+TEST_F(TestScriptFixture, TestScript1CheckMockReadWriteTimes) {
+	EXPECT_CALL(mockWriter, execute)
+		.Times(100);
+
+	EXPECT_CALL(mockReader, execute)
+		.Times(100);
+
+	mockApp.FullWriteAndReadCompare(FIRST_TEST_SCRIPT_NAME);
 }
 
 #endif
