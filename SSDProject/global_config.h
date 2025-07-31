77085a4381e07c522e5608bca0e0653dc8469db3
kgm-dsedu2 <kimkm18@gmail.com>
2025-07-31 11:02:02 +0900

    Debugging 용 Log 추가 - 사용법 : 구성을 TEST_CONFIG로 설정 - 추가 내용 : SSD, NAND Class 내 에러 및 정상 동작 로깅 추가

--- SSDProject/global_config.h
+++ SSDProject/global_config.h
@@ -15,6 +15,16 @@ const char ZERO_PADDING = '0';
 
 const std::string OUTPUT_FILE = "ssd_output.txt";
 
+#ifdef _TEST_CONFIG
+#include <iostream>
+
+#define TEST_LOGGER(msg) \
+    std::cout << "[LOG] " << __func__ << ": " << msg << std::endl;
+#else
+#define TEST_LOGGER(msg) \
+    ;
+#endif
+
 #ifdef _DEBUG
 #define DEBUG_ASSERT(cond, msg)                     \
         do {                                        \
@@ -22,6 +32,9 @@ const std::string OUTPUT_FILE = "ssd_output.txt";
                 throw std::runtime_error(msg);      \
             }                                       \
         } while (0)
+#elsif _TEST_CONFIG
+#define DEBUG_ASSERT(cond, msg) \
+    TEST_LOGGER(msg)
 #else
 #define DEBUG_ASSERT(cond, msg) do { (void)(cond); (void)(msg); } while (0)
 #endif
